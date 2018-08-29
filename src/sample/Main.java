2a0e3792e6269dce7458777a48f151e614bd4355
luffyxin <937443942@qq.com>
2018-08-29 13:55:03 +0800

    读取excel完成

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,10 @@
 package sample;
 
+import com.luffyxin.bean.Person;
+import com.luffyxin.controller.PersonOverviewController;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -14,7 +18,31 @@ public class Main extends Application {
     private Stage primaryStage;
 
     private BorderPane rootLayout;
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
 
+    /**
+     * Constructor
+     */
+    public Main() {
+        // Add some sample data
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    /**
+     * Returns the data as an observable list of Persons.
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -33,7 +61,7 @@ public class Main extends Application {
         try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("RootLayout.fxml"));
+            loader.setLocation(Main.class.getResource("../com/luffyxin/view/RootLayout.fxml"));
             rootLayout = (BorderPane) loader.load();
 
             // Show the scene containing the root layout.
@@ -49,14 +77,23 @@ public class Main extends Application {
     /**
      * Shows the person overview inside the root layout.
      */
+    /**
+     * Shows the person overview inside the root layout.
+     */
     public void showPersonOverview() {
         try {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("PersonOverview.fxml"));
+            loader.setLocation(Main.class.getResource("../com/luffyxin/view/PersonOverview.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
+
             // Set person overview into the center of root layout.
             rootLayout.setCenter(personOverview);
+
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
