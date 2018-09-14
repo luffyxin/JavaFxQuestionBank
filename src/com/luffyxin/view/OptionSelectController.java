e4da6d85c5b8ff6ee940630281f0900aff08dade
luffyxin <937443942@qq.com>
2018-09-14 21:45:04 +0800

    radiosTest02

--- src/com/luffyxin/view/OptionSelectController.java
+++ src/com/luffyxin/view/OptionSelectController.java
@@ -6,7 +6,9 @@ import javafx.scene.control.RadioButton;
 
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,7 +17,11 @@ import java.util.List;
 public class OptionSelectController {
 
     @FXML
-    private AnchorPane ap;
+    private AnchorPane  ap;
+
+    public OptionSelectController(){
+        Context.controllers.put(this.getClass().getSimpleName(),this);
+    }
 
     @FXML
     public void initialize(){
@@ -30,8 +36,13 @@ public class OptionSelectController {
                 new RadioButton("ArrayList集合"),
                 new RadioButton("字符流、字符缓冲流")
         );
-        for(int i=0;i<radios.size();i++)
-        ap.getChildren().add(i,radios.get(i));
+      for(int i=0;i<radios.size();i++){
+          ap.getChildren().add(radios.get(i));
+          AnchorPane.setTopAnchor(radios.get(i),i*20.0);
+      }
+
+
+
     }
 
 }
